d8cdefcf53ec98b92539cea46601e460c51cd9f8
SpoilerSnow <3499045227@qq.com>
2026-05-29 23:18:00 +0800

    2

--- .godot/mono/temp/obj/Debug/TheInsatiable.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/TheInsatiable.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TheInsatiable")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bde3f16cf55c5e5c390db1e5c3032ddc724864bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+377789de9f0b9f59ccceb28761c0513d91f89fb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TheInsatiable")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TheInsatiable")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
